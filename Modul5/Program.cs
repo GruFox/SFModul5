5b6d08d4a699817bd602cb50626c2bfb6003bc49
Gru <energimen@yandex.ru>
2025-01-16 18:38:04 +0100

    Добавьте файлы проекта.

---
+++ Modul5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Modul5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string[] Dishes) User;
+
+            Console.Write("Введите Ваше имя: ");
+            User.Name = Console.ReadLine();
+
+
+        }
+    }
+}
